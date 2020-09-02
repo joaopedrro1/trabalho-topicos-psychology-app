6353c570adf8791147a8ee52b2d4acc34878d6e8
João Pedro <jpdasp@gmail.com>
2020-09-02 00:08:41 -0300

    Primeiro commit com uma tela

---
+++ src/Login.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './Login.css';
+
+function Login() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <div class="container">
+          <img src={logo} className="App-logo" alt="logo" />
+          </div>
+        </header>
+      </div>
+    );
+  }
+
+  export default Login;
